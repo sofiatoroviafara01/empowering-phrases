fd224bf977634c1db580ab52980aede8d49e2ce9
luadimensional <apellidoyo930@gmail.com>
2025-07-31 15:29:52 +0200

    se agrega la carpeta de css de form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
  import { useState } from "react";
-
+ import './Form.css'
 function MotivationalPhrases() {
   const [phrases, setPhrases] = useState([]);
   const [author, setAuthor] = useState("");
