03c8203e5a0486f36fb72af75bbf02788c0bedf1
AngelaBello-creator <angelabellomedina@gmail.com>
2025-08-06 15:47:40 +0200

    agregamos form al main

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Form from "./components/Form/Form"
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +12,7 @@ function App() {
   return (
     <>
       <div>
+            <Form/>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
